978afe72435cce8165c9c5375a6f8d80886a6912
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-20 21:05:11 +0000

    everything but random

--- app.rb
+++ app.rb
@@ -24,3 +24,27 @@ get("/square_root/results") do
   @the_root = @the_num ** 0.5
   erb(:square_root_results)
 end
+
+get("/payment/new") do
+  erb(:payments)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_f
+  @principal = params.fetch("user_pv").to_f
+
+  @formatted_apr = format('%.4f%%', @apr)
+  @formatted_years = @years.to_i
+  @formatted_principal = @principal.to_fs(:currency)
+
+  @r = @apr/(100*12)
+  @n = @years * 12
+  @numerator = @r * @principal
+  @denominator = 1 - ((1 + @r) ** (-@n))
+  @payment = @numerator/@denominator
+
+  @formatted_payment = @payment.to_fs(:currency)
+  erb(:payment_results)
+
+end

a2aa3012216a065265a4db9ba6469ef66ee23124
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-20 20:25:28 +0000

    square and square root

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get ("/square/new") do
+erb(:square)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_square = @the_num * @the_num
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_root = @the_num ** 0.5
+  erb(:square_root_results)
 end
